cd6eebf3615e9cbe710538fe83515bd86cb00f49
zbrennan5642 <zbrennan5642@gmail.com>
2025-10-21 13:01:38 +0100

    Nearly added Trending this week will come back after doint exercise 2

--- src/api/tmdb-api.jsx
+++ src/api/tmdb-api.jsx
@@ -72,7 +72,7 @@ export const getMovie = (args) => {
 
   export const getTrendingThisWeekMovies = () => {
     return fetch(
-    `https://api.themoviedb.org/3/trending/movie/week?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
     ).then((response) => {
       if (!response.ok){
         return response.json().then((error) => {
